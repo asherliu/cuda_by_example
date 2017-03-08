0960c3431d84cdc36fb6e0c5ca4657720f00b0e2
asherliu <asherliu@gwu.edu>
2017-03-08 09:29:19 -0500

    update

--- chapter04/adder/add_loop_gpu.cudafe2.c
+++
@@ -1,7 +0,0 @@
-# 1 "add_loop_gpu.cudafe1.gpu"
-# 212 "/c1/apps/gcc/4.9.0/lib/gcc/x86_64-unknown-linux-gnu/4.9.0/include/stddef.h" 3
-typedef unsigned long size_t;
-#include "crt/host_runtime.h"
-void *memcpy(void*, const void*, size_t); void *memset(void*, int, size_t);
-
-#include "add_loop_gpu.cudafe2.stub.c"
